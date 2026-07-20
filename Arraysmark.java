38fdc44825ba866acf524e77fd1745b541963503
hinatajeni13 <hinatajeni13@gmail.com>
2026-07-20 14:36:02 +0530

    Create Arraysmark.java

---
+++ Arraysmark.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int[]marks={70,80,60,90,50};
+        String[]subject={"tamil","english","maths","science","physis"};
+        for (int i=0;i<marks.length;i++){
+            System.out.println(subject[i]+"="+marks[i]);
+        }
+    }
+}
