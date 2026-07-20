7b3d649e528c11db9f38ceb79d805249c77f17c7
hinatajeni13 <hinatajeni13@gmail.com>
2026-07-20 14:39:45 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int[]totalmarks={450,480,350,410,388};
+        String[]name={"deva","anusuya","jeni","dharshini","brundha"};
+        for (int i=0;i<totalmarks.length;i++){
+            System.out.println(name[i]+"="+totalmarks[i]);
+        }
+    }
+}
