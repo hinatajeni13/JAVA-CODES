37f8f921de442e3eda55d896f0156c257aacbde0
hinatajeni13 <hinatajeni13@gmail.com>
2026-07-20 14:59:09 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        String str="jenifer";
+        int tamil=60;
+        int english=70;
+        int maths=80;
+        int science=90;
+        int social=100;
+    System.out.println("name="+str);
+    System.out.println("tamil="+tamil);
+    System.out.println("english="+english);
+    System.out.println("maths="+maths);
+    System.out.println("science="+science);
+    System.out.println("social="+social);
+        }
+    }
